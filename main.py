22d19234d3c1135d9838776f47464f37444de8eb
KennyThrug <51040230+KennyThrug@users.noreply.github.com>
2021-12-11 22:26:43 -0600

    Make Next and Prev work

--- main.py
+++ main.py
@@ -3,12 +3,44 @@ import pygame
 from pygame import mixer
 import os
 from tkinter import *
+import random
 
 
 app = Flask(__name__)
 
 mixer.init()
-mixer.music.load("Songs/blowing_raspberries.mp3")
+
+curSong = -1
+listofFiles = []
+path = os.getcwd() + "/Songs"
+def randomize():
+    global listofFiles
+    random.shuffle(listofFiles)
+def getAllFiles():
+    global path
+    global listofFiles
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            listofFiles.append(os.path.join(root,file))
+def getNextSong():
+    global curSong
+    global listofFiles
+    curSong += 1
+    if(curSong >= len(listofFiles)):
+        curSong = 0
+    elif(curSong < 0):
+        curSong = len(listofFiles)
+    return listofFiles[curSong]
+def getPrevSong():
+    global curSong
+    global listofFiles
+    curSong -= 1
+    if(curSong >= len(listofFiles)):
+        curSong = 0
+    elif(curSong < 0):
+        curSong = len(listofFiles)-1
+    return listofFiles[curSong]
+
 
 @app.route('/')
 def index():
@@ -18,40 +50,56 @@ def index():
 def addSongToPlaylist(indexOfSong, indexOfPlaylist):
     print("todo")
 
+isPlaying = False
+isPaused = True
+
+
+getAllFiles()
+
+print(listofFiles)
+
 #Play Button
 @app.route('/play/')
 def play():
-    mixer.music.play()
+    global isPaused
+    global isPlaying
+    global songList
+    if(isPlaying == False):
+        randomize()
+        mixer.music.load(getNextSong())
+        mixer.music.play()
+        isPlaying = True
+    elif(isPaused == True):
+        mixer.music.pause()
+        isPaused = False
+    else:
+        mixer.music.unpause()
+        isPaused = True
     return render_template('MainControl.html',data=True) 
 
 @app.route('/stop/')
 def stop():
-    mixer.music.play()
+    global isPlaying
+    mixer.music.stop()
+    isPlaying = False
     return render_template('MainControl.html',data=True) 
 
 @app.route('/prev/')
 def prev():
+    mixer.music.load(getPrevSong())
     mixer.music.play()
-    return render_template('MainControl.html',data=True) 
-
-#Pause Button
-@app.route('/pause/')
-def pause(varName=None):
-    mixer.music.pause()
+    isPlaying = True
+    isPaused = False
     return render_template('MainControl.html')
 
 #Next Button
 @app.route('/next/')
 def skip():
+    mixer.music.load(getNextSong())
+    mixer.music.play()
+    isPlaying = True
+    isPaused = False
     return render_template('MainControl.html')
 
-#Randomize Playlist
-def randomizePlaylist():
-    print("todo")
-
-#Start A Playlist
-def startPlaylist():
-    print("todo")
-
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True,host="0.0.0.0")
