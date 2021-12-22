9fe1b4d5889e15406b7d41d4d6fe4e408fde7a2a
KennyThrug <51040230+KennyThrug@users.noreply.github.com>
2021-12-22 11:49:10 -0600

    Play Song after Song is Finished

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pygame import mixer
 import os
 from tkinter import *
 import random
+import threading
 
 
 app = Flask(__name__)
@@ -40,6 +41,10 @@ def getPrevSong():
     elif(curSong < 0):
         curSong = len(listofFiles)-1
     return listofFiles[curSong]
+def hasSong():
+    global listofFiles
+    global curSong
+    return not (len(listofFiles) == curSong)
 
 
 @app.route('/')
@@ -69,20 +74,21 @@ def play():
         mixer.music.load(getNextSong())
         mixer.music.play()
         isPlaying = True
-    elif(isPaused == True):
-        mixer.music.pause()
         isPaused = False
+    elif(isPaused == False):
+        mixer.music.pause()
+        isPaused = True
     else:
         mixer.music.unpause()
-        isPaused = True
-    return render_template('MainControl.html',data=True) 
+        isPaused = False
+    return render_template('MainControl.html') 
 
 @app.route('/stop/')
 def stop():
     global isPlaying
     mixer.music.stop()
     isPlaying = False
-    return render_template('MainControl.html',data=True) 
+    return render_template('MainControl.html') 
 
 @app.route('/prev/')
 def prev():
@@ -101,5 +107,24 @@ def skip():
     isPaused = False
     return render_template('MainControl.html')
 
+def getShouldPause():
+    global isPlaying
+    global isPaused
+    global mixer
+    if(isPlaying and not isPaused):
+        if(not mixer.music.get_busy()):
+            return True
+    return False
+
+def runApp():
+    app.run(debug=True,host="0.0.0.0")
+def checkForNextSong():
+    while(True):
+        if(getShouldPause()):
+            skip()
+
 if __name__ == '__main__':
-  app.run(debug=True,host="0.0.0.0")
+    t1 = threading.Thread(target=checkForNextSong)
+    t1.start()
+    runApp()
+    t1.join()
